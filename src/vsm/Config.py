be55840684f5f0d2a556a1896f2ed3510149daae
marcfyk <marcjohnfong@gmail.com>
2020-03-14 14:59:23 +0800

    update __init__.py

---
+++ src/vsm/Config.py
@@ -0,0 +1,22 @@
+
+
+class Config:
+
+
+    def __init__(self, dictionaryFilePath="", postingsFilePath="", documentMapFilePath=""):
+        self.dictionaryFilePath = dictionaryFilePath
+        self.postingsFilePath = postingsFilePath
+        self.documentMapFilePath = documentMapFilePath
+
+    def __hash__(self):
+        return hash(self.dictionaryFilePath, self.postingsFilePath, self.documentMapFilePath)
+
+    def __eq__(self, o):
+        checkDictionaryFilePath = self.dictionaryFilePath == o.dictionaryFilePath
+        checkPostingsFilePath = self.postingsFilePath == o.postingsFilePath
+        checkDocumentMapFilePath = self.documentMapFilePath == o.documentMapFilePath
+        return checkDictionaryFilePath and checkPostingsFilePath and checkDocumentMapFilePath
+
+    def __repr__(self):
+        return f"dictionary -> {self.dictionaryFilePath}, postings -> {self.postingsFilePath}, documents -> {self.documentMapFilePath}"
+
---
+++ src/vsm/ConfigFileHandler.py
@@ -0,0 +1,28 @@
+from os import path
+from pickle import load
+from pickle import dump
+
+from .Config import Config
+
+class ConfigFileHandler:
+
+    def __init__(self, configFile, directory=""):
+        self.configFile = configFile
+        self.directory = directory
+        self.configFilePath = path.join(directory, configFile)
+
+    def read(self):
+        configFilePath = self.configFilePath
+
+        try:
+            with open(configFilePath, "rb") as f:
+                config = load(f)
+            return config
+        except EOFError:
+            return Config()
+
+    def write(self, config):
+        configFilePath = self.configFilePath
+        with open(configFilePath, "wb") as f:
+            dump(config, f)
+
